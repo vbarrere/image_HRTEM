0c7c3cefdcc665b653271ebffa09b08dd902b9f4
vbarrere <victor.barrere45@gmail.com>
2026-03-04 10:38:32 +0100

    Creation of run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export path_xyz="/home/victor.barrere@crmd.cnrs-orleans.fr/Documents/Data/Data_xyz/"
+export path_processed="/home/victor.barrere@crmd.cnrs-orleans.fr/Documents/Data/Data_processed/"
+export path_new_xyz=$path_processed"XYZ/"
